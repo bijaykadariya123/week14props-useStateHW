55f3dec2cf379daf1007358dc399de38e3788ce3
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-08-02 15:40:06 -0400

    remark

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,13 +2,13 @@
 
 function Displaypage(props){
     const randomQuestion = props.questionIs
-console.log(randomQuestion[0]);
+console.log(randomQuestion[0]);// I can see this in console but I am not able to see in my display page, I did see the data structure, I am not sure what piece am i missing?
 
 
     return(
         <div>
             <h3>This is Display Page</h3>
-            <p> question:{randomQuestion.question}</p>
+            <p> question:{randomQuestion[0].question}</p>
         </div>
     )
 }
